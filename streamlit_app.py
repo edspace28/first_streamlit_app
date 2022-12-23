70595dbfab48c36a05717e6733f9f73a3006eccb
Ed Garnett <121287497+edspace28@users.noreply.github.com>
2022-12-23 09:31:03 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents' New Healthy Diner")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Full English')
+streamlit.text('Fruit Topped Waffles')
+streamlit.text('Pancakes with Maple Syrup')
